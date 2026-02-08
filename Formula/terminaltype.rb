16fef63e64419707ba16c20c58f6f52aacd9e6d4
sharann-del <sharannmanojkumar@gmail.com>
2026-02-08 09:24:42 +0530

    Rename to terminaltype

--- Formula/terminaltype.rb
+++ Formula/terminaltype.rb
@@ -1,8 +1,8 @@
-class Monkeytypetui < Formula
+class Terminaltype < Formula
   desc "Monkeytype-inspired terminal typing test"
-  homepage "https://github.com/sharann-del/monkeytypetui"
-  url "https://registry.npmjs.org/monkeytypetui/-/monkeytypetui-2.0.0.tgz"
-  sha256 "e492847093067696583355c0c59ea51c3acf33d27ac7993d42cf47f2b7a706a4"
+  homepage "https://github.com/sharann-del/terminaltype"
+  url "https://registry.npmjs.org/terminaltype/-/terminaltype-3.0.0.tgz"
+  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
   license "MIT"
 
   depends_on "node"
