8a0bea408049beef6ff1ebdc775e37cc1bd1d91f
sharann-del <sharannmanojkumar@gmail.com>
2026-02-08 22:21:14 +0530

    update terminaltype to 4.0.5

--- Formula/terminaltype.rb
+++ Formula/terminaltype.rb
@@ -1,8 +1,8 @@
 class Terminaltype < Formula
   desc "Terminal-based typing test"
   homepage "https://github.com/sharann-del/terminaltype"
-  url "https://registry.npmjs.org/terminaltype/-/terminaltype-4.0.4.tgz"
-  sha256 "79adee8f86213107180f5dccb14913e62ab540e28b8f10095839e4b7face20d5"
+  url "https://registry.npmjs.org/terminaltype/-/terminaltype-4.0.5.tgz"
+  sha256 "251dd4069d874e523f4af3280214e70da6860e2eddd5f90778adbf4191291394"
   license "MIT"
 
   depends_on "node"
